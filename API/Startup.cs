b2faa25aaa5df32007030a47a2f73513742db895
Shailendra Singh <shailendrasingh823@gmail.com>
2020-03-30 10:36:24 +0530

    End of Section 3(Axios+ Semantic)

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,18 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy",policy =>
+                {
+                   policy.AllowAnyHeader().AllowAnyMethod()
+                   .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,11 +54,12 @@ namespace API
             }
 
            // app.UseHttpsRedirection();
-
+           
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
