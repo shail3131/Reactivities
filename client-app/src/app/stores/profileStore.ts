74777d88b97a6eb55639e885c7c193de219db8da
shail3131 <shailendrasingh823@gmail.com>
2020-06-04 08:01:57 +0530

    End of Section 18 (Edit Profile)

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,118 +1,120 @@
-import { toast } from 'react-toastify';
-import { RootStore } from './rootStore';
-import { observable, action,runInAction,computed } from 'mobx';
-import { IProfile, IPhoto } from '../models/profile';
+import { IProfile, IPhoto } from './../models/profile';
+import {RootStore} from './rootStore';
+import { observable, action, runInAction, computed } from 'mobx';
 import agent from '../api/agent';
+import { toast } from 'react-toastify';
 
 export default class ProfileStore {
-
     rootStore: RootStore
-
-    constructor(rootStore:RootStore)
-    {
-        this.rootStore=rootStore
+    constructor(rootStore: RootStore) {
+        this.rootStore = rootStore;
     }
 
-    @observable profile : IProfile | null = null;
-
+    @observable profile: IProfile | null = null;
     @observable loadingProfile = true;
-
     @observable uploadingPhoto = false;
-
     @observable loading = false;
+   
 
-    @computed get isCurrentUser(){
-        if(this.rootStore.userStore.user && this.profile){
-            return this.rootStore.userStore.user.username=== this.profile.username;
-        }
-        else{
+    @computed get isCurrentUser() {
+        if (this.rootStore.userStore.user && this.profile) {
+            return this.rootStore.userStore.user.username === this.profile.username
+        } else {
             return false;
         }
     }
 
-
-    @action loadProfile = async (username : string) => {
+    @action loadProfile = async (username: string) => {
         this.loadingProfile = true;
-        try{
-           const profile = await agent.Profiles.get(username);
-           runInAction(() =>{
-               this.profile = profile;
-               this.loadingProfile = false;
-           })
-        }catch(error){
-              runInAction(() =>{
-                  this.loadingProfile = false;
-              })
-              console.log(error);
+        try {
+            const profile = await agent.Profiles.get(username);
+            runInAction(() => {
+                this.profile = profile;
+                this.loadingProfile = false;
+            })
+        } catch (error) {
+            runInAction(() => {
+                this.loadingProfile = false;
+            })
+            console.log(error)
         }
-
     }
 
-    @action uploadPhoto = async (file:Blob) => {
+    @action uploadPhoto = async (file: Blob) => {
         this.uploadingPhoto = true;
-        try{
-             const photo = await agent.Profiles.uploadPhoto(file)
-             
-             runInAction(() =>{
-                    if(this.profile){
-                     this.profile.photos.push(photo);
-                     if(photo.isMain && this.rootStore.userStore.user){
-                         this.rootStore.userStore.user.image = photo.url;
-                         this.profile.image = photo.url
-                     }
-                 }
-                 this.uploadingPhoto = false;
-             })
-        }
-        catch(error){
-           console.log(error);
-           toast.error('Problem uploading photo');
-           runInAction(() => {
-             this.uploadingPhoto = false;
-           })
+        try {
+            const photo = await agent.Profiles.uploadPhoto(file);
+            runInAction(() => {
+                if (this.profile) {
+                    this.profile.photos.push(photo);
+                    if (photo.isMain && this.rootStore.userStore.user) {
+                        this.rootStore.userStore.user.image = photo.url;
+                        this.profile.image = photo.url
+                    }
+                }
+                this.uploadingPhoto = false;
+            })
+        } catch (error) {
+            console.log(error);
+            toast.error('Problem uploading photo')
+            runInAction(() => {
+                this.uploadingPhoto = false;
+            })
         }
     }
 
-    @action setMainPhoto = async (photo : IPhoto) => {
+    @action setMainPhoto = async (photo: IPhoto) => {
         this.loading = true;
-            try{
+        try {
             await agent.Profiles.setMainPhoto(photo.id);
             runInAction(() => {
                 this.rootStore.userStore.user!.image = photo.url;
-                this.profile!.photos.find( a => a.isMain)!.isMain = false;
-                this.profile!.photos.find( a => a.id === photo.id)!.isMain = true;
-                this.profile!.image=photo.url;
-                console.log('this.loading before='+this.loading);
+                this.profile!.photos.find(a => a.isMain)!.isMain = false;
+                this.profile!.photos.find(a => a.id === photo.id)!.isMain = true;
+                this.profile!.image = photo.url;
                 this.loading = false;
-                console.log('this.loading after='+this.loading)
             })
-        }catch(error){
-        toast.error("Problem sting photo as main");
-        runInAction(() =>{
-            this.loading = false;
-        })
-          
+        } catch (error) {
+            toast.error('Problem setting photo as main');
+            runInAction(() => {
+                this.loading = false;
+            })
         }
-
     }
 
-    @action deletePhoto =  async (photo : IPhoto) => {
-
+    @action deletePhoto = async (photo: IPhoto) => {
         this.loading = true;
+        try {
+            await agent.Profiles.deletePhoto(photo.id);
+            runInAction(() => {
+                this.profile!.photos = this.profile!.photos.filter(a => a.id !== photo.id);
+                this.loading = false;
+            })
+        } catch (error) {
+            toast.error('Problem deleting the photo');
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    @action updateProfile = async (profile : Partial<IProfile>) => {
+        await agent.Profiles.updateProfile(profile);
+
         try{
 
-           await agent.Profiles.deletePhoto(photo.id);
+            runInAction(() =>{
+                if(profile.displayName !== this.rootStore.userStore.user?.displayName){
+                    this.rootStore.userStore.user!.displayName =profile.displayName!;                   
+                }
+    
+                this.profile = {...this.profile!,...profile};
+            })
 
-           runInAction(() =>{
-               this.profile!.photos = this.profile!.photos.filter( a => a.id !== photo.id);
-               this.loading = false;
-           })
-
-        } catch(error){
-           toast.error('Problem deleting the photo');
-           runInAction(() =>{
-               this.loading = false;
-           })
+        }catch(error){
+            toast.error('Problem  updating profile');
         }
+
+       
     }
 }
--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,97 +1,94 @@
-import React, { useContext, useState } from 'react'
-import { Tab, Header, Card, Image, Button, Grid } from 'semantic-ui-react'
-import { RootStoreContext } from '../../app/stores/rootStore'
-import  PhotoUploadWidget  from '../../app/common/photoUpload/PhotoUploadWidget'
-import { observer } from 'mobx-react-lite'
+import React, { useContext, useState } from 'react';
+import { Tab, Header, Card, Image, Button, Grid } from 'semantic-ui-react';
+import { RootStoreContext } from '../../app/stores/rootStore';
+import PhotoUploadWidget from '../../app/common/photoUpload/PhotoUploadWidget';
+import { observer, Observer } from 'mobx-react-lite';
 
 export const ProfilePhotos = () => {
-  
-    const rootStore = useContext(RootStoreContext);
+  const rootStore = useContext(RootStoreContext);
+  const {
+    profile,
+    isCurrentUser,
+    uploadPhoto,
+    uploadingPhoto,
+    setMainPhoto,
+    deletePhoto,
+    loading
+  } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [deleteTarget, setDeleteTarget] = useState<string | undefined>(
+    undefined
+  );
 
-    const {
-        profile,
-        isCurrentUser,
-        uploadPhoto,
-        uploadingPhoto,
-        setMainPhoto,
-        deletePhoto,
-        loading} = rootStore.profileStore;
+  const handleUploadImage = (photo: Blob) => {
+    uploadPhoto(photo).then(() => setAddPhotoMode(false));
+  };
 
-    const [addPhotoMode,setAddPhotoMode] = useState(false);
-
-    const [target,setTarget] = useState<string | undefined>(undefined);
-
-    const [deleteTarget,setDeleteTarget] = useState<string | undefined>(undefined);
-
-    const handleUploadImage = (photo : Blob) =>{
-        uploadPhoto(photo).then(() => setAddPhotoMode(false))
-    }
-
-    return (
-        <Tab.Pane>
-            <Grid>
-                <Grid.Column width={16} style = {{paddingBottom:0}} > 
-                    <Header floated = 'left' icon='image' content ='Photos'/>
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16} style={{ paddingBottom: 0 }}>
+          <Header floated='left' icon='image' content='Photos' />
+          {isCurrentUser && (
+            <Button
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+              floated='right'
+              basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget
+              uploadPhoto={handleUploadImage}
+              loading={uploadingPhoto}
+            />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile &&
+                profile.photos.map(photo => (
+                  <Card key={photo.id}>
+                    <Image src={photo.url} />
                     {isCurrentUser && (
-                        <Button 
-                        floated='right' 
-                        basic 
-                        content={addPhotoMode ? 'Cancel' : 'Add Photo'}
-                        onClick={() => setAddPhotoMode(!addPhotoMode)}
-                      />
+                      <Button.Group fluid widths={2}>
+                        <Button
+                          onClick={e => {
+                            setMainPhoto(photo);
+                            setTarget(e.currentTarget.name);
+                          }}
+                          name={photo.id}
+                          disabled={photo.isMain}
+                          loading={loading && target === photo.id}
+                          basic
+                          positive
+                          content='Main'
+                        />
+                        <Button
+                          name={photo.id}
+                          disabled={photo.isMain}
+                          onClick={(e) => {
+                            deletePhoto(photo);
+                            setDeleteTarget(e.currentTarget.name)
+                          }}
+                          loading={loading && deleteTarget === photo.id}
+                          basic
+                          negative
+                          icon='trash'
+                        />
+                      </Button.Group>
                     )}
-                </Grid.Column>
-                <Grid.Column width={16}>
-
-                    {addPhotoMode ?(
-                        <PhotoUploadWidget  
-                        uploadPhoto = {handleUploadImage} 
-                        loading = {uploadingPhoto}/>
-                    ) : (
-                        <Card.Group itemsPerRow = {5}>
-                        {profile &&  
-                        profile.photos.map((photo) =>(
-                        <Card key = {photo.id}>
-                        <Image src = {photo.url}/>
-
-                        { isCurrentUser &&(
-                           <Button.Group fluid widths={2}>
-                               <Button 
-                               name= {photo.id}
-                               onClick = {(e) => {
-                                setMainPhoto(photo);
-                                setTarget(e.currentTarget.name)
-                               }} 
-                               disabled = {photo.isMain}
-                               loading = {loading && target === photo.id}
-                               basic 
-                               positive 
-                               content='Main'/>
-                               <Button
-                                name = {photo.id}
-                                disabled = {photo.isMain}
-                                onClick = {(e) => {
-                                    deletePhoto(photo);
-                                    setDeleteTarget(e.currentTarget.name)
-                                }}
-
-                                loading = {loading && deleteTarget === photo.id}
-                                basic 
-                                negative 
-                                icon='trash'/>
-                           </Button.Group>
-                        )}
-                        
-                        </Card>
-                   ))}
-                   </Card.Group>
-                    ) }
-                       
-                </Grid.Column>
-            </Grid>          
-           
-        </Tab.Pane>
-    )
+                  </Card>
+                ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
 };
 
-export default observer(ProfilePhotos)
+
+
+export default observer(ProfilePhotos);
